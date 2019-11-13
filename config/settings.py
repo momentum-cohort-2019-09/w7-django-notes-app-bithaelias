58625d69f6af4caa546201bcd01395d114e5087e
Bitha Elias <bitha.jordan@gmail.com>
2019-11-13 17:06:16 -0500

    deploying to heroku

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bitha-notes',
-        'USER': 'bitha-notes’,
+        'USER': 'bitha-notes',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
